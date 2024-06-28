54e1a5a2446fba0bdc95db9239100700424b2a45
Bulat Gabdrakhmanov <gabdrakhmanov.br@gmail.com>
2024-06-28 15:29:46 +0700

    fix install-service

--- install-service.sh
+++ install-service.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
+
 sudo cp ./hamsterbot.service /etc/systemd/system/hamsterbot.service
 sudo systemctl daemon-reload
 sudo systemctl enable hamsterbot.service
-sudo systemctl start hamsterbot.service
-echo "hamsterbot service status: $(sudo systemctl status hamsterbot.service)"
